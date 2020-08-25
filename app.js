8a7a564189bc1eb116a59cf85f0571ad1964aecf
pachi <pachicodes@gmail.com>
2020-08-25 15:04:53 -0400

    added infos to pachicopdes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
+  res.innerHTML = "";
   const user = fetch("https://api.github.com/users/pachicodes").then((user) =>
     user.json()
   );
@@ -13,11 +14,18 @@ btn.addEventListener("click", function () {
 
     const picture = document.createElement("img");
     picture.setAttribute("src", avatar);
+    picture.setAttribute("class", "avatar");
 
     const nickElement = document.createElement("h3");
     const userLogin = document.createTextNode(nick);
+
+    const descriptionElement = document.createElement("p");
+    const userBio = document.createTextNode(description);
+
     res.append(picture);
     nickElement.append(userLogin);
     res.append(nickElement);
+    descriptionElement.append(userBio);
+    res.append(descriptionElement);
   });
 });
